f6d27351f05c7aaf391df390814ccaaea52c3f4d
FabioCividini <fabiocividini22@gmail.com>
2021-11-23 15:32:25 +0100

    esercizio finito

--- src/org/generation/italy/geometria/Main.java
+++ src/org/generation/italy/geometria/Main.java
@@ -1,10 +1,28 @@
 package org.generation.italy.geometria;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		
-
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("Inserire la BASE del rettangolo: ");
+		int base = scanner.nextInt();
+		
+		System.out.print("Inserire l'ALTEZZA del rettangolo: ");
+		int altezza = scanner.nextInt();
+		
+		Rettangolo areaRettangolo = new Rettangolo(base, altezza);
+		int area = areaRettangolo.calcolaArea();
+		System.out.println("L'AREA del rettangolo è: " + area + "cm");
+		
+		Rettangolo perimetroRettangolo = new Rettangolo(base, altezza);
+		int perimetro = perimetroRettangolo.calcolaPerimetro();
+		System.out.println("Il PERIMETRO del rettangolo è: " + perimetro + "cm");
+		
+		scanner.close();
 	}
 
 }
--- src/org/generation/italy/geometria/Rettangolo.java
+++ src/org/generation/italy/geometria/Rettangolo.java
@@ -1,5 +1,26 @@
 package org.generation.italy.geometria;
 
 public class Rettangolo {
-
+	//attributi interi
+	int base;
+	int altezza;
+	
+	//costruttore con parametri
+	Rettangolo(int base, int altezza){
+		this.base = base;
+		this.altezza = altezza;
+	}
+	
+	//metodo calcolaArea
+	int calcolaArea() {
+		int calcolaArea = base * altezza;
+		return calcolaArea;
+	}
+	
+	//metodo calcolaPerimetro
+	int calcolaPerimetro() {
+		int calcolaPerimetro = base * 2 + altezza * 2;
+		return calcolaPerimetro;
+	}
+	
 }
